4c641f927fa848348d0d95973dde2ede1962a698
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-21 20:29:33 +0900

    コメントアウト

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ bot.add('/', function (session) {
 });
 
 // Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
+// var server = restify.createServer();
+// server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+// server.listen(process.env.port || 3978, function () {
+//     console.log('%s listening to %s', server.name, server.url);
+// });
