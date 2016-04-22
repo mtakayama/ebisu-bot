902dcf0cdccadd7a8465b1f0236b5fbc9190875a
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-22 23:18:59 +0900

    修正

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'ebisu', appSecret: 'e159fb9b61304f0e9148cd1e86a14b68' });
-bot.add('/', function (session) {
-    session.send('Hello World');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(request, response) {
+  response.send('hello!!!';
 });
 
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(app.get('port'), function () {
-    console.log('%s listening to %s', server.name, server.url);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
