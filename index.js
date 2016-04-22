f66d873672c1d82218708fdd40af7ef1322873ef
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-22 23:46:05 +0900

    修正

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 var express = require('express');
 var app = express();
 
-<<<<<<< HEAD
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: process.env.app_id, appSecret: process.env.app_secret });
 bot.add('/', function (session) {
     session.send('Hello World');
-=======
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-
-app.get('/', function(request, response) {
-  response.send('hello!!!');
->>>>>>> master
 });
 
 
