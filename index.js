0cc5fa480c1e258653bb4d0d4b2d2388d96332a0
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-24 00:24:22 +0900

    修正

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var bot_id = process.env.bot_id || '28:655e0a0d-a998-4f26-aa5c-7310fdac1f11';
+var app_id = process.env.app_id || 'ebisu';
+var app_secret = process.env.app_secret || 'e159fb9b61304f0e9148cd1e86a14b68';
+
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: process.env.app_id, appSecret: process.env.app_secret });
+var bot = new builder.BotConnectorBot({ appId:app_id, appSecret: app_secret });
 bot.add('/', function (session) {
     session.send('hello!!!!');
 });
