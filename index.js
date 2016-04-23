e0ee6167a877c8b6dd9d5b7835de07b3b35b35b1
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-23 23:36:02 +0900

    修正

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-console.log('kita');
+const restify = require('restify');
+const skype = require('skype-sdk');
+const builder = require('botbuilder');
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: process.env.app_id, appSecret: process.env.app_secret });
-bot.add('/', function (session) {
-    session.send('hello!!!!');
+var bot_id = process.env.bot_id || 'e77a844b-17c1-4d4b-8238-6f680dc9373d';
+var app_id = process.env.app_id || 'ebisu';
+var app_secret = process.env.app_secret || 'e159fb9b61304f0e9148cd1e86a14b68';
+
+// Initialize the BotService
+const botService = new skype.BotService({
+    messaging: {
+        botId: bot_id,
+        serverUrl : "https://apis.skype.com",
+        requestTimeout : 15000,
+        appId: app_id,
+        appSecret: app_secret
+    }
 });
 
+// Create bot and add dialogs
+var bot = new builder.SkypeBot(botService);
+bot.add('/', function (session) {
+   session.send('Hello World');
+   console.log('kita!!!');
+});
 
-var server = restify.createServer();
-
-function respond(req, res, next) {
-    res.send('Hello Restify!');
-}
-server.get('/', respond);
-
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-
+// Setup Restify Server
+const server = restify.createServer();
 var port = process.env.PORT || 5000;
+server.post('/v1/chat', skype.messagingHandler(botService));
 server.listen(port, function () {
-    console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url);
 });
