29dc44c6caae6f65cbd8e8b1766f150e972d9c4d
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-24 01:04:18 +0900

    修正

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const skype = require('skype-sdk');
 const builder = require('botbuilder');
 
 var bot_id = process.env.bot_id || '655e0a0d-a998-4f26-aa5c-7310fdac1f11';
-var app_id = process.env.app_id || 'ebisu';
-var app_secret = process.env.app_secret || 'e159fb9b61304f0e9148cd1e86a14b68';
+var app_id = process.env.app_id || '655e0a0d-a998-4f26-aa5c-7310fdac1f11';
+var app_secret = process.env.app_secret || 'SRWy8ivbBacwOjSJ8axsCtP';
 
 // Initialize the BotService
 const botService = new skype.BotService({
@@ -17,16 +17,17 @@ const botService = new skype.BotService({
     }
 });
 
-// Create bot and add dialogs
-var bot = new builder.SkypeBot(botService);
-bot.add('/', function (session) {
-   session.send('Hello World');
-   console.log('kita!!!');
+botService.on('contactAdded', (bot, data) => {
+    bot.reply(`Hello ${data.fromDisplayName}!`, true);
+});
+
+botService.on('personalMessage', (bot, data) => {
+    bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
 });
 
 // Setup Restify Server
 const server = restify.createServer();
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 server.post('/api/messages', skype.messagingHandler(botService));
 server.listen(port, function () {
    console.log('%s listening to %s', server.name, server.url);
