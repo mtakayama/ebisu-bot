cbd074fcc05d2bd2533e0ceccc06729b0b68aedd
takayama minoru <takayama.minoru@interfactory.co.jp>
2016-04-23 18:38:01 +0900

    修正

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+console.log('kita');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: process.env.app_id, appSecret: process.env.app_secret });
 bot.add('/', function (session) {
-console.log('kita');
+    session.send('hello!!!!');
 });
 
 
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
